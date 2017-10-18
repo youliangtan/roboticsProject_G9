4ec9c0b2054b5b6f61d90aa58bda8ca23ed5f7fb
PhilipPuan123 <32876956+PhilipPuan123@users.noreply.github.com>
2017-10-19 02:02:27 +0800

    Add Main Part

---
+++ MainFrameProject/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h.
+
+using namespace std;
+
+int main()
+{
+    /*user start to input number of bread, number of eggs, number of cups
+
+    if(Nb==2)
+    {
+      bread1();       take bread to a certain position in tray
+      bread2();       take 2nd bread to different position in tray
+      signal1();      after breads taken, write a txt file to tell user the process of taking bread is completed.
+    }
+
+    else
+    {
+     bread1();
+     signal1();
+    }
+
+    if(Ne==2)
+    {
+      egg1();    Put 1st eggs into a position in tray
+      egg2();    Put 2nd eggs into different position in tray
+      signal2();  send signal so that user aware the 2nd process is completed
+
+    }
+
+    else
+    {
+     egg1();
+     signal2();
+    }
+
+    cup();      Put a cup to a position in tray
+    signal3()   send signal so that user aware the process is completed*/
+
+    return 0;
+}
