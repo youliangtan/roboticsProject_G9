f379fee56319dcc7551391bcf1b3e357c9fc363d
EeMeng <32729683+EeMeng@users.noreply.github.com>
2017-10-20 19:35:32 +0800

    Motion.h changes

--- MainFrameProject/Motion/motion.cpp
+++ MainFrameProject/Motion/motion.cpp
@@ -1,16 +1,17 @@
 #include "motion.h"
+#define NO_OF_MOTORS 6
 
 using namespace std;
 
 int arm_motion(mode Mode,int pick_pos, int tray_pos)
 {
-    int pos;
+    int pick_pos, tray_pos;
     string pick_filename,tray_filename;
     vector< vector<int> > vect;
     switch(Mode)
     {
-    case bread: pick_filename = "bread" + pick_pos + ".csv"; tray_filename = "bread_tray" + tray_pos + ".csv"; break;
-    case egg: pick_filename = "egg.csv"; tray_filename = "egg_tray" + tray_pos + ".csv"; break;
+    case bread: pick_filename = "bread_pick" + pick_pos + ".csv"; tray_filename = "bread_tray" + tray_pos + ".csv"; break;
+    case egg: pick_filename = "egg_pick.csv"; tray_filename = "egg_tray" + tray_pos + ".csv"; break;
     case drinks: pick_filename = "drinks.csv"; break;
     }
 
@@ -20,10 +21,13 @@ int arm_motion(mode Mode,int pick_pos, int tray_pos)
     //Iterator
     for(auto it = vect.begin(); it != vect.end(); it++)
     {
-        for(auto it2 = it->begin(); it2 != it2->end(); it2++)
+        for(int counter = 1; counter <= NO_OF_MOTORS; counter++)
         {
-            static counter = 1;
-            dxl_write_word(counter, /*MOVE*/, *it2);
+            int time = it[NO_OF_MOTORS + 1]; //Time is stored in the column after the last motor
+            int theta = abs(it[counter] - dxl_read_word(counter, CUR_POS));
+            int velocity = theta/time;
+            dxl_write_word(counter, MOV_VEL, velocity);
+            dxl_write_word(counter, END_POS, it[counter]);
         }
         while(/* TODO: waiting to reach final position*/)
     }
@@ -35,12 +39,13 @@ int arm_motion(mode Mode,int pick_pos, int tray_pos)
     //Iterator
     for(auto it = vect.begin(); it != vect.end(); it++)
     {
-        for(auto it2 = it->begin(); (it2 + 1) != it->end(); it2++)
-            static int counter = 1;
-            int time = it->end();
-
-            dxl_write_word(counter,/*VELOCITY*/, *it2);
-            dxl_write_word(counter, /*MOVE*/, *it2);
+        for(int counter = 1; counter <= NO_OF_MOTORS; counter++)
+        {
+            int time = it[NO_OF_MOTORS + 1]; //Time is stored in the column after the last motor
+            int theta = abs(it[counter] - dxl_read_word(counter, CUR_POS));
+            int velocity = theta/time;
+            dxl_write_word(counter, MOV_VEL, velocity);
+            dxl_write_word(counter, END_POS, it[counter]);
         }
         while(/* TODO: waiting to reach final position*/)
     }
--- MainFrameProject/Motion/motion.h
+++ MainFrameProject/Motion/motion.h
@@ -3,7 +3,31 @@
 
 #include <string>
 #include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
 #include "array_storage.h"
+#include "dynamixel.h"
+
+#pragma comment(lib, "dynamixel.lib")
+
+#define TORQUE_SWITCH	24
+#define END_POS			30
+#define MOV_VEL			32
+#define CUR_POS			36
+#define MOVING			46
+
+#define PORTNUM			5
+#define BAUDNUM			1
+
+#define JOINT_1			1
+#define JOINT_2			2
+#define JOINT_3			3
+#define JOINT_4			4
+#define GRIPPER_LEFT	5
+#define GRIPPER_RIGHT	6
+
+#define MAX_VEL			90	//To set the max angular velocity
 
 enum mode {egg, bread, drinks};
 
