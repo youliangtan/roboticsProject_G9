ff57838646f4049ee53fb45505d5458a3325f632
youliang <max@nutonomy.com>
2017-10-26 18:11:01 +0800

    new UI

--- UI_display/app.py
+++ UI_display/app.py
@@ -81,6 +81,6 @@ def send():
 
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', port=8800)
+    # app.run(host= '0.0.0.0', port=8800)
     # app.run(host= '192.168.31.160', port=8888, debug=False)
-    # app.run(host= '192.168.8.103', port=8080, debug=False)
+    app.run(host= '10.27.7.251', port=8080, debug=False)
