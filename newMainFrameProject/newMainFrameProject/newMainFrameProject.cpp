2a85d8673706841822b4dbb1b98c1dce3f8d5c69
PhilipPuan123 <32876956+PhilipPuan123@users.noreply.github.com>
2017-10-26 09:54:46 +0800

    add

--- newMainFrameProject/newMainFrameProject/newMainFrameProject.cpp
+++ newMainFrameProject/newMainFrameProject/newMainFrameProject.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-
+#include "motion.h"
 #include "ArdSensor.h"
 #include<stdio.h>
 #include<fstream>
@@ -13,9 +13,9 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <sstream>
-//#include "dynamixel.h"
+#include "dynamixel.h"
 
-//#pragma comment(lib, "dynamixel.lib")
+#pragma comment(lib, "dynamixel.lib")
 using namespace std;
 
 #define PORT_NUM 6
@@ -30,7 +30,10 @@ struct choicelist
 	char drink;
 	char a, b;
 	int numbread;
+	char d;
+	
 	int numegg;
+	
 }choice;
 
 
@@ -44,16 +47,16 @@ int writeUI(string status) {
 	return 0;
 }
 
-int ultraRead(int position, int N, int numbread)
+int ultraRead( ArdSensor* ard1, int N, int numbread)
 {
-	int distance = position;
+	
 	int startPos;
 	/*Read ultrasonic reading of bread*/
 
 	// if cumalative value of bread smaller than 4, sensor1 being called to scan 1st raw
-	if (N < 4)
-	{
-		//int distance = ard1->ardRead("U1");
+	
+	
+		int distance = ard1->ardRead("U");
 		
 		if (distance>0 && distance< 10)
 		{
@@ -65,23 +68,23 @@ int ultraRead(int position, int N, int numbread)
 			startPos = 2;
 			return startPos;
 		}
-		else if (distance < 30)
+		else 
 		{
 			startPos = 3;
 			return startPos;
 		}
 
-		else
+	/*	else
 		{
-			/*ask ultrasonic sensor scan the distance again*/
-			startPos = ultraRead(position, N, numbread);
+			/*ask ultrasonic sensor scan the distance again
+			startPos = ultraRead(ArdSensor* ard1, N, numbread);
 			return startPos;
-		}
+		}*/
 
-	}
+	
 
 	//if cumlative value smaller than 7 but bigger than 3, 2nd sensor is called to scan 2nd raw.
-	else if (N<7)
+	/*else if (N<7)
 	{
 		//int distance = ard1->ardRead("U2");
 		
@@ -104,20 +107,20 @@ int ultraRead(int position, int N, int numbread)
 		else
 		{
 			/*ask ultrasonic sensor scan the distance again*/
-			startPos = ultraRead(position, N, numbread);
+/*			startPos = ultraRead(position, N, numbread);
 			return startPos;
 		}
 
-	}
+	}*/
 
 
 	//once cumaltive value of N exceed 6, N will be initialised to 0 again
-	else 
+/*	else 
 	{
 		N = 0;
 		N += numbread;
 		return ultraRead(position, N, numbread);
-	}
+	} */
 
 	
 
@@ -130,8 +133,11 @@ int main() {
 	//return array address of selection
 	int N = 0;
 	int Position;
-	int quit=3;
-
+	int quit = 3;
+	
+	
+	
+	printf("hi.\n");
 	//read selection from choice.txt
 	//return array address of selection
 	while (quit != 0)
@@ -139,82 +145,99 @@ int main() {
 		ifstream myfile(ChoiceFilePath);
 		if (myfile.is_open())
 		{
-			myfile >> choice.drink >> choice.a >> choice.numbread >> choice.b >> choice.numegg;
+
+			myfile >> choice.drink >> choice.a >> choice.numbread >> choice.b >> choice.numegg >> choice.d;
+		
+
+			if (choice.d == NULL)
+			{
+				//wait until user input the string and UI send the string to txt file
+				printf("file is not ready.\n");
+				Sleep(1000);
+			}
+			else if (choice.d =='.')
+			{
+				printf("Preparing Meal.\n");
+				Sleep(1000);
+				//delete contents of myfile.
+				myfile.close();
+				//perform actions..
+				char Drink = choice.drink;
+				if (Drink == 'c')
+				{
+					printf("coffee ordered by user\n");
+				}
+				else
+				{
+					printf("tea ordered by user\n");
+				}
+				int numBread = choice.numbread;
+				printf("%d bread ordered by user\n", numBread);
+				int numEgg = choice.numegg;
+				printf("%d egg ordered by user\n", numEgg);
 
 
 
-			myfile.close();
+				ArdSensor ard1(PORT_NUM);
+
+				//cumalative value of bread
+				N += numBread;
+
+
+
+				/*for bread*/
+				int i;
+				int startPos;
+
+				for (i = 1; i <= numBread; i++)
+				{
+					
+					startPos = ultraRead(&ard1, N, numBread);
+					printf("Performing number %d motion of bread.\n", i);
+					writeUI("toast");
+					
+					arm_motion(bread, startPos, NULL);
+					printf("number %d motion of bread is done.\n", i);
+
+				}
+
+				/*for egg*/
+				for (i = 1; i <= numEgg; i++)
+				{
+					writeUI("eggs");
+
+					printf("Performing number %d motion of egg.\n", i);
+					Sleep(5000);
+
+					arm_motion(egg, NULL, i + 1);
+					printf("%dst motion of egg is performed.\n", i);
+
+				}
+
+				/*for drinks*/
+				writeUI("drinks");
+				arm_motion(drinks, NULL, NULL);
+				Sleep(5000);
+
+				//finish and ready to serve!!
+				writeUI("finish");
+				printf("finish.\n");
+				printf("input 0 to quit or else press other value.\n");
+				scanf_s("%d", &quit);
+
+			}
+			
 		}
+
+
+
 		else
 		{
 			cout << "Unable to open file";
-
+			
 		}
-
-		char Drink = choice.drink;
-		if (Drink == 'c')
-		{
-			printf("coffee ordered by user\n");
-		}
-		else
-		{
-			printf("tea ordered by user\n");
-		}
-		int numBread = choice.numbread;
-		printf("%d bread ordered by user\n", numBread);
-		int numEgg = choice.numegg;
-		printf("%d egg ordered by user\n", numEgg);
-
-
-
-		//ArdSensor ard1(PORT_NUM);
-
-		//cumalative value of bread
-		N += numBread;
-
-
-
-		/*for bread*/
-		int i;
-		int startPos;
-
-		for (i = 1; i <= numBread; i++)
-		{
-			printf("Input a manual position.\n");
-			scanf_s("%d", &Position);
-			startPos = ultraRead(Position, N, numBread);
-			printf("Performing number %d motion of bread.\n", i);
-			writeUI("Bread");
-			Sleep(5000);
-			//arm_motion(Bread, startPos, NULL);
-			printf("number %d motion of bread is done.\n", i);
-
-		}
-
-		/*for egg*/
-		for (i = 1; i <= numEgg; i++)
-		{
-			writeUI("Egg");
-
-			printf("Performing number %d motion of egg.\n", i);
-			Sleep(5000);
-
-			//arm_motion(Egg, NULL, i + 1);
-			printf("%dst motion of egg is performed.\n", i);
-
-		}
-
-		/*for drinks*/
-		writeUI("Drink");
-		//arm_motion(Drink, NULL, NULL);
-		Sleep(5000);
-
-		//finish and ready to serve!!
-		writeUI("finish");
-		printf("finish.\n");
-		printf("input 0 to quit or else press other value.\n");
-		scanf_s("%d", &quit);
-		
 	}
 	return 0;
 }
+
+		
